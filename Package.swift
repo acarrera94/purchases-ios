a95b23b48dbdbd60497b52b18a25c5668805bf92
Andre Carrera <acarrera94@gmail.com>
2019-09-20 14:47:32 -0600

    2.6.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "Purchases", targets: ["Purchases"])
     ],
     dependencies: [
-//    .package(url: "https://github.com/AliSoftware/OHHTTPStubs", .exact("8.0.0")),
-//    .package(url: "https://github.com/Quick/Nimble", .exact("8.0.4"))
+        //    .package(url: "https://github.com/AliSoftware/OHHTTPStubs", .exact("8.0.0")),
+        //    .package(url: "https://github.com/Quick/Nimble", .exact("8.0.4"))
     ],
     targets:[
     .target(name: "Purchases", dependencies: [], path: "Purchases", exclude: [], sources: nil, publicHeadersPath: nil)
