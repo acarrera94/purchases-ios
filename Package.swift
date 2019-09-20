7fe61d860d0acb57ba34537f65a62033ed436223
Andre Carrera <acarrera94@gmail.com>
2019-09-20 14:35:00 -0600

    added package file

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Purchases",
+    products: [
+        .library(name: "Purchases", targets: ["Purchases"])
+    ],
+    dependencies: [
+    .package(url: "https://github.com/AliSoftware/OHHTTPStubs", .exact("8.0.0")),
+    .package(url: "https://github.com/Quick/Nimble", .exact("v8.0.2"))
+    ],
+    targets:[
+    .target(name: "Purchases", dependencies: [], path: "Purchases", exclude: [], sources: nil, publicHeadersPath: nil)
+//        .target(name: "SwiftyBeaver", dependencies: [], path: "Purchases")
+//        .testTarget(name: "PurchasesTests", dependencies: ["Purchases"])
+    ]
+)
